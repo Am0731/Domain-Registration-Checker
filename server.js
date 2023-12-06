da8604fe70408d4cd1196a4a17e3d86ef2299de1
Am0731 <425598740@qq.com>
2023-12-06 22:40:32 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('.'));
+
+app.post('/queryDomain', async (req, res) => {
+    const { domainName } = req.body;
+    console.log('收到域名查询请求:', domainName);
+
+    // 拆分域名
+    const parts = domainName.split('.');
+    const name = parts[0];
+    const suffix = parts.slice(1).join('.');
+
+    // 构建查询接口的 URL
+    const queryUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
+
+    try {
+        const response = await fetch(queryUrl);
+        const result = await response.json();
+        res.send({ message: "查询成功", result: result });
+    } catch (error) {
+        console.error('查询错误:', error);
+        res.status(500).send({ message: "服务器错误" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`服务器运行在 http://localhost:${port}`);
+});
